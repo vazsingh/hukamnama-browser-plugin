9e2f2149f944152c960bd6bf8a407158e0a0c905
vazsingh <vazsingh@gmail.com>
2022-04-25 19:56:45 +0100

    Minor changes

--- popup.js
+++ popup.js
@@ -1,6 +1,6 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   var checkPageButton = document.getElementById('checkPage');
-  checkPageButton.addEventListener('click', function() {
+  checkPageButton.addEventListener('click', function () {
 
 
 
